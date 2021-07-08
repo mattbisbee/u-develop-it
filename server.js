b6576d3e5fb4da7ebfac730d7bf3181846a55f7e
Biznaz <aldhelm7@gmail.com>
2021-07-08 11:26:31 -0700

    added code to server js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,12 +10,29 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username
+    user: 'root',
+    // Your MySQL password
+    password: 'Biznaz0814!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
 //test route to see if the server was functioning properly
 // app.get('/', (req, res) => {
 //   res.json({
 //     message: 'Hello World'
 //   });
 // });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
